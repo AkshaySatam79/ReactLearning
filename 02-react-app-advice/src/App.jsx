ae82715a64e1c2b47e096fe7fb0b1ea76c026fc9
AkshaySatam79 <akshaysatam@79gmail.com>
2026-07-13 23:15:06 +0530

    Initial commit

---
+++ 02-react-app-advice/src/App.jsx
@@ -0,0 +1,39 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
+  );
+}
---
+++ 04-pizza-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const name = "Akshay79";
+  return (
+    <>
+      <h1>Hello React!!!</h1>
+      <p>
+        THIS IS ME: <span>{name}</span>
+      </p>
+    </>
+  );
+}
+
+export default App;
